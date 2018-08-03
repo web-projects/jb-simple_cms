47591f70d6734318bf36ec394ebae4be79ded148
Jonnie Ringo <web.projects@yahoo.com>
2018-08-02 23:49:20 -0700

    Added associations

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,14 +1,9 @@
 class AdminUser < ApplicationRecord
 
   #self.table_name = "admin_users"
-  attr_accessor :first_name
 
-  def last_name
-    @last_name
-  end
-
-  def last_name=(value)
-    @last_name = value
-  end
+  has_and_belongs_to_many :pages
+  has_many :section_edits
+  has_many :sections, :through => :section_edits
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_03_011402) do
+ActiveRecord::Schema.define(version: 2018_08_03_063120) do
 
   create_table "admin_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "first_name", limit: 25
@@ -21,6 +21,12 @@ ActiveRecord::Schema.define(version: 2018_08_03_011402) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["username"], name: "index_admin_users_on_username"
+  end
+
+  create_table "admin_users_pages", id: false, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.integer "admin_user_id"
+    t.integer "page_id"
+    t.index ["admin_user_id", "page_id"], name: "index_admin_users_pages_on_admin_user_id_and_page_id"
   end
 
   create_table "pages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
@@ -35,6 +41,15 @@ ActiveRecord::Schema.define(version: 2018_08_03_011402) do
     t.index ["subject_id"], name: "index_pages_on_subject_id"
   end
 
+  create_table "section_edits", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.integer "admin_user_id"
+    t.integer "section_id"
+    t.string "summary"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["admin_user_id", "section_id"], name: "index_section_edits_on_admin_user_id_and_section_id"
+  end
+
   create_table "sections", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.integer "page_id"
     t.string "name"
@@ -45,6 +60,14 @@ ActiveRecord::Schema.define(version: 2018_08_03_011402) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["page_id"], name: "index_sections_on_page_id"
+  end
+
+  create_table "sections_edits", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.integer "admin_user_id"
+    t.integer "section_id"
+    t.string "summary"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "subjects", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
